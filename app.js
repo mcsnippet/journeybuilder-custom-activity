d48788d01d0187695a644973216675cd8af3ccc9
mcsnippet <73090652+mcsnippet@users.noreply.github.com>
2023-09-08 21:51:15 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const helmet = require('helmet');
-const httpErrors = require('http-errors');
-const logger = require('morgan');
-const path = require('path');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-const activityRouter = require('./routes/activity');
+require('dotenv').config(); // JL dotenv: Loads environment variables from a .env file.
+const cookieParser = require('cookie-parser'); // JL cookieParser: Middleware for parsing cookies.
+const express = require('express'); // JL express: The Express.js framework for building web applications.
+const helmet = require('helmet'); // JL helmet: Middleware for adding various HTTP security headers.
+const httpErrors = require('http-errors'); // JL http-errors: Helps in creating HTTP error objects.
+const logger = require('morgan'); // JL logger: Middleware for logging HTTP requests and responses.
+const path = require('path'); // JL path: Node.js module for working with file paths.
+const bodyParser = require('body-parser'); // JL body-parser: Middleware for parsing the request body.
+const routes = require('./routes/index'); // JL routes: Imports a module for handling routes.
+const activityRouter = require('./routes/activity'); // JL activityRouter: Imports a module specifically for custom activity routes.
 
-const app = express();
+const app = express(); // This line creates an instance of the Express.js application and assigns it to the app variable. 
 app.use(
+// app.use() is a method in Express used to mount middleware functions. 
+// Middleware functions are functions that have access to the request (req) and response (res) objects and can perform various tasks,
+// such as modifying the request, response, or terminating the request-response cycle.
+
+/*
+The code below configures the helmet middleware to set a Content Security Policy (CSP) for your Express.js application. 
+The CSP defines rules for what types of content can be loaded and from which sources. 
+In this case, it restricts content loading to the same origin by default and allows embedding in frames from specific Salesforce Marketing Cloud domains, enhancing the security of your custom activity.
+*/
   helmet({
     contentSecurityPolicy: {
       directives: {
@@ -22,24 +31,34 @@ app.use(
 );
 
 // view engine setup
+/*
+In summary, these two lines are configuring your Express.js application to use the "pug" template engine for rendering views, and it specifies the directory 
+where these view templates can be found ('views' in the same directory as your app.js file). 
+This allows you to use Pug templates to generate dynamic HTML content in your application's views.
+*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // JL This middleware is used to parse incoming JSON data from HTTP request bodies.
+app.use(express.urlencoded({ extended: false })); // JL This middleware is used to parse incoming URL-encoded data from HTTP request bodies.
 app.use(cookieParser());
 app.use(bodyParser.raw({
   type: 'application/jwt',
-}));
+})); // This middleware is used to parse incoming request bodies with a specific content type ('application/jwt' in this case). 
 
 app.use(express.static(path.join(__dirname, 'public')));
+// This middleware serves static files, such as HTML, CSS, JavaScript, images, and other assets, from a directory named "public."
 
 // serve config
 app.use('/config.json', routes.config);
 
 // custom activity routes
-app.use('/journey/execute/', activityRouter.execute);
+/*
+This line sets up a route for the "execute" action of your custom activity in Salesforce Marketing Cloud Journey Builder. 
+Requests to /journey/execute/ are handled by the activityRouter.execute middleware.
+*/
+app.use('/journey/execute/', activityRouter.execute); 
 app.use('/journey/save/', activityRouter.save);
 app.use('/journey/publish/', activityRouter.publish);
 app.use('/journey/validate/', activityRouter.validate);
